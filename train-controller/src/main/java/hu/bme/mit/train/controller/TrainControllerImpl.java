b6433b01f85a5e9d909897f27130d3b85801a168
Dávid Pál <dpal17@student.aau.dk>
2021-03-18 15:16:14 +0100

    Feature implemented

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,12 +2,16 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 5;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-
+	private Timer timer = new Timer();
+	private boolean run=false;
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
@@ -43,7 +47,27 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;
+		runReferenceSpeedChange();
 	}
+	public void setRun(){
+		this.run=!this.run;
+	}
+	private void runReferenceSpeedChange()
+	{
+		setRun();
+		TimerTask joystickTask=new TimerTask() {
+			@Override
+			public void run() {
+				followSpeed();
+				System.out.println("Joystick position handled");
+			}
+		};
+		while(run){
+			timer.scheduleAtFixedRate(joystickTask,10,2000);
+		}
+		timer.cancel();
+		System.out.println("System joystick handling shutdown");
 
+	}
 }
